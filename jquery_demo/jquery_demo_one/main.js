79b4eea34c0a16c889be1c1f4e8bf5c76af5500d
heguohui2018 <heguohui2017@outlook.com>
2020-02-20 11:47:44 +0800

    jquery选择器

--- jquery_demo/jquery_demo_one/main.js
+++ jquery_demo/jquery_demo_one/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	$("#a").css({'background':"yellow"});
 	$(".a").css({'background':"red"});
-	$("#b p").css({'background':"yellow"});
+	$("#b p").css({'background':"green"});
 });
 
