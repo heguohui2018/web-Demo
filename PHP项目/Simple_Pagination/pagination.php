27005c58e7a38a67d68426a3fdc84c60bb128a4a
heguohui2018 <heguohui2017@outlook.com>
2020-09-17 17:47:08 +0800

    简单分页程序

--- PHP项目/Simple_Pagination/pagination.php
+++ PHP项目/Simple_Pagination/pagination.php
@@ -2,15 +2,17 @@
 include("db.php");
 
 /* 每页限制条数 */
-$limit = 2;
+$limit = 10;
 
 if (isset($_GET['page'])) {
-	/* 页数 */
-	$page = $_GET['page'] - 1;
+	/* 当前第几页页数 */
+	$page = $_GET['page'];
 	/* 偏移量 */
-	$offset = $page * $limit;
+	$offset = ($page - 1) * $limit;
 } else {
+	/* 当前第几页页数 */
 	$page = 0;
+	/* 偏移量 */
 	$offset = 0;
 }
 
@@ -18,15 +20,17 @@ if (isset($_GET['page'])) {
 /* 返回 users2 表中数据条数 */
 $sql = "SELECT * FROM users2";
 $sql_result = mysqli_query($connection, $sql);
-$row = mysqli_num_rows($sql_result);
+/* 总行数 */
+$total_rows = mysqli_num_rows($sql_result);
 
 /* 字符串 和 数字比较，字符串会转化为数字 */
-if ($row > $limit) {
+if ($total_rows > $limit) {
 	/* 需要多少页 */
 	/* call 取整 */
-	$number_of_pages = ceil($row / $limit);
+	$number_of_pages = ceil($total_rows / $limit);
 } else {
-	$pages = 1;
+	/* 当前第几页页数 */
+	$page = 1;
 	$number_of_pages = 1;
 }
 
